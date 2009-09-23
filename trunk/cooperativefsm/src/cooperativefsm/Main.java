f18d33d1d860cebc90c843dd426639aeeed1fc07
svanerac@gmail.com <svanerac@gmail.com@5f65ece2-a45f-11de-a118-c7387b1e4f66>
2009-09-23 10:14:02 +0000

    bozza della struttura del core by luca e carlo

--- trunk/cooperativefsm/src/cooperativefsm/Main.java
+++ trunk/cooperativefsm/src/cooperativefsm/Main.java
@@ -12,16 +12,23 @@ package cooperativefsm;
  *
  * @author Cominardi Luca, Ferrari Alessandro, Svanera Carlo
  */
-public class Main {
+public class Main {     //è la classe che gestisce la simulazione
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 
         // TODO code application logic here
+
+
+    Fsm fsm1,fsm2;
+
+
+    fsm1= Init.init();
+
     }
 
 }
