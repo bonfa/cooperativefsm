77e7517444984534f76b7e82c7866264aa7c5dfc
francesco.bonfadelli <francesco.bonfadelli@5f65ece2-a45f-11de-a118-c7387b1e4f66>
2009-11-18 13:17:06 +0000

    Adesso l'esecuzione dovrebbe gestire tutto in maniera corretta.Corretto anche l'ultimo bug

--- trunk/cooperativefsm/src/cooperativefsm/Messaggi.java
+++ trunk/cooperativefsm/src/cooperativefsm/Messaggi.java
@@ -15,7 +15,7 @@ public interface Messaggi {
     /**Messaggi utilizzati dalla classe Interazione*/
     public final static String STATO_CORRENTE="STATO CORRENTE SIMULAZIONE:";
     public final static String TRANSIZIONI_ABILITATE="TRANSIZIONI ABILITATE:";
-    public final static String MENU_TRANSIZIONI_ABILITATE="ID\tFSM_1_INIZ\tFSM_1_FIN\tFSM_2_INIZ\tFSM_2_FIN";
+    public final static String MENU_TRANSIZIONI_ABILITATE="ID\tFSM_1(INIZ - FIN)\tFSM_2(INIZ - FIN)";
     public final static String SCEGLI_TRANSIZIONE="Scegliere la transizione da fare scattare:[Selezione per ID; -1 PER USCIRE]";
     public final static String LA_ACCENDIAMO="Sei sicuro?:";
     public final static String NO_TRANSIZIONI_ABILITATE="Non ci sono transizioni abilitate allo scatto!";
@@ -24,7 +24,7 @@ public interface Messaggi {
     public final static String ERRORE="ERRORE!";
     public final static String ERR_TRANSIZIONI_USCENTI="Le transizioni uscenti non sono state correttamente settate!";
     public final static String ERR_TRANSIZIONI_SINCRONE="Le relazioni sincrone non sono state settate!";
-    public final static String NIENTE="-\t\t-\t\t";
+    public final static String NIENTE=" NULL ";
     public final static String MENU_STATO_CORRENTE="SC_FSM_1\tSC_FSM_2";
     public final static String ABILITAZIONE="Vuoi far scattare questa transizione?";
     public final static int ESCI=-1;
